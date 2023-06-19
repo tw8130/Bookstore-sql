50768029cebeaafe044878de74196196f4ff12b6
Edward Mutahi <edward.ngugi@thejitu.com>
2023-06-19 15:37:55 +0300

    Utility token for a user once they log in. Lasts for 1 hour

---
+++ src/utilis/token.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const tokenGenerator = async(data) => {
+    let token = jwt.sign(data, process.env.SECRET);
+    return token;
+};
+
+//test a token is legit and not tampered with
+function tokenVerifier(token) {
+    return jwt.verify(token, process.env.SECRET), { expiresIn: '1h' };
+}
+
+//token when they login, protected routes e.g getAllBooks if logged in
+module.exports = {
+    tokenGenerator,
+    tokenVerifier
+};
