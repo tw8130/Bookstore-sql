e6ef8acab54825fac3ffc7bed0957491a94406c0
tw8130 <mwangitabitha2020@gmail.com>
2023-06-27 13:43:09 +0300

    made changes to the server js

--- src/routes/loansRouter.js
+++ src/routes/loansRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getMembersWhoBorrowed, returnBook, borrowBook,checkOverdueBooks } = require('../controllers/loansController');
+const { getMembersWhoBorrowed, getAMemberWhoBorrowed, returnBook, borrowBook, checkOverdueBooks } = require('../controllers/loansController');
 const router = express.Router();
 const { authenticateToken } = require('../middlewares/auth');
 
@@ -7,16 +7,17 @@ const { authenticateToken } = require('../middlewares/auth');
 
 
 router.use(authenticateToken)
-router.post('/check-overdue-books', async (req, res) => {
+router.post('/check-overdue-books', async(req, res) => {
     try {
-      await checkOverdueBooks();
-      res.status(200).json({ message: 'Overdue books checked successfully.' });
+        await checkOverdueBooks();
+        res.status(200).json({ message: 'Overdue books checked successfully.' });
     } catch (error) {
-      console.error('Error checking overdue books:', error);
-      res.status(500).json({ error: 'An error occurred while checking overdue books.' });
+        console.error('Error checking overdue books:', error);
+        res.status(500).json({ error: 'An error occurred while checking overdue books.' });
     }
-  });
+});
 router.get('/members/books-borrowed', getMembersWhoBorrowed)
+router.get('/member/book-borrow', getAMemberWhoBorrowed)
 router.post('/return', returnBook);
 router.post('/borrow', borrowBook);
 // router.get('/', getBorrowedBook)
